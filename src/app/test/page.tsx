644cac233f94956453f753eb5d0eb9e938f9e308
Enrico <enrico.fritz@student.reutlingen-university.de>
2026-03-09 15:30:28 +0100

    tests

---
+++ src/app/test/page.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import Component from "@/src/components/Button";
+import AppNavBar from "@/src/components/AppNavBar";
+
+export default function Test(){
+
+
+    return(
+        <>
+        
+            <h1>test</h1>
+            <Component></Component>
+            <AppNavBar />
+        
+        </>
+    )
+}
